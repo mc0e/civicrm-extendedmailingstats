99c869f7adc7ac45044340b1efdd7603e1dce7ff
andrewmcnaughton <andrew@mcnaughty.com>
2013-09-25 22:44:02 +1000

    remove some junk files and some that got renamed

--- api/v3/Mailingstats/Cron.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// This can be run like
-//
-// drush -r /var/www/htdocs -l example.com -u 1 civicrm-api mailingstats.cron auth=0 -y > /dev/null
-
-function civicrm_api3_mailingstats_cron($params) {
-  _mailingstats_cron($params);
-}
