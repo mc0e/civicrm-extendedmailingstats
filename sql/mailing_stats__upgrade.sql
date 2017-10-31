37cde797f80259038df6a3803f4bff8e9f5b24a1
zosyk <alekcandrzosyk@gmail.com>
2017-10-31 23:04:50 +0200

    added unique columns

--- sql/mailing_stats__upgrade.sql
+++ sql/mailing_stats__upgrade.sql
@@ -8,6 +8,10 @@ ALTER TABLE `agc_report_mailing_stats`
   ADD COLUMN `hotmail_delivered` FLOAT UNSIGNED NULL DEFAULT NULL,
   ADD COLUMN `hotmail_opened` FLOAT UNSIGNED NULL DEFAULT NULL,
   ADD COLUMN `hotmail_clicked_total` FLOAT UNSIGNED NULL DEFAULT NULL,
-  ADD COLUMN `hotmail_clicked_unique` FLOAT UNSIGNED NULL DEFAULT NULL
+  ADD COLUMN `hotmail_clicked_unique` FLOAT UNSIGNED NULL DEFAULT NULL,
+  ADD COLUMN `opened_unique` FLOAT UNSIGNED NULL DEFAULT NULL AFTER `opened`,
+  ADD COLUMN `gmail_opened_unique` FLOAT UNSIGNED NULL DEFAULT NULL AFTER `gmail_opened`,
+  ADD COLUMN `yahoo_opened_unique` FLOAT UNSIGNED NULL DEFAULT NULL AFTER `yahoo_opened`,
+  ADD COLUMN `hotmail_opened_unique` FLOAT UNSIGNED NULL DEFAULT NULL AFTER `hotmail_opened`
 ;
 
